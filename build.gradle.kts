2d3fcf22ea4be57135d87183fea6c4de2658f18f
Taskeren <r0yalist@outlook.com>
2025-11-14 03:15:35 +0800

    Added maven central publishing, untested

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+
+    `maven-publish`
+    id("com.gradleup.nmcp.aggregation") version "1.2.0"
 }
 
 group = "cn.elytra"
@@ -21,7 +24,29 @@ tasks.test {
     useJUnitPlatform()
 }
 
+java {
+    withSourcesJar()
+}
+
 kotlin {
     jvmToolchain(8)
     explicitApi()
+}
+
+fun prop(name: String): String? = findProperty(name) as? String ?: System.getenv(name)
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+nmcpAggregation {
+    centralPortal {
+        username = prop("MAVEN_CENTRAL_USERNAME")
+        password = prop("MAVEN_CENTRAL_PASSWORD")
+        publishingType = "USER_MANAGED"
+    }
 }
